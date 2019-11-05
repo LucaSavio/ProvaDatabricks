8eb0d7fccaa49437ef3017419dd95509ea7afc17
Luke DataWalker <42337672+LucaSavio@users.noreply.github.com>
2019-11-05 17:03:30 +0100

    Prova per Databricks

---
+++ Prova_Databricks.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+class Prova():
+    def random():
+
+        R = np.random.randn(3,4)
+        print(R)
+
+        return True
+
+    def ciao():
+        message = 'Questa è solo una prova'
+        print(message)
+        return True
+        
